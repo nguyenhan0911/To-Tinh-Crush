37704f74b82459646596834c454bdf554396e9b3
duytrieu235 <86992508+duytrieu235@users.noreply.github.com>
2021-07-06 11:52:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao emm lại yêu anhh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiềuuuuu 😘😘',
     messDesc: 'Ngại gì mà không inbox cho tuii ngayy cái cậu nàyyyy <333',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
